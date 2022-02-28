d29fcc44c54e86adb9d290e9e5124ca18ac25f3f
dianedelallee <diane.delallee@gmail.com>
2022-02-28 16:43:57 +0100

    fix(Formula): fix url path

--- Formula/qoqa_cli.rb
+++ Formula/qoqa_cli.rb
@@ -3,8 +3,7 @@ class QoqaCli < Formula
   homepage "https://github.com/dianedelallee/QoQaCommandLine"
   version "0.1"
 
-
-  url "https://github.com/dianedelallee/QoQaCommandLine/archive/main.zip", :using => :curl
+  url "https://github.com/dianedelallee/homebrew-QoQaCommandLine/archive/refs/tags/v0.1.zip", :using => :curl
 
   def install
     bin.install "bin/qoqa_cli"
