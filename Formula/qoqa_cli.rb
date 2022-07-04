4e608d8ee05775d91ed31be71fe3a0be3f9b1a49
dianedelallee <diane.delallee@gmail.com>
2022-07-04 15:51:27 +0200

    update offer to use v2 mosaic API - remove jobs because this is not working for now

--- Formula/qoqa_cli.rb
+++ Formula/qoqa_cli.rb
@@ -1,9 +1,9 @@
 class QoqaCli < Formula
-  desc "Command Line interface to get information about QoQa (live offers, jobs)"
+  desc "Command Line interface to get information about QoQa (live offers)"
   homepage "https://github.com/dianedelallee/QoQaCommandLine"
-  version "0.3"
+  version "0.4"
 
-  url "https://github.com/dianedelallee/homebrew-QoQaCommandLine/archive/refs/tags/v0.3.zip", :using => :curl
+  url "https://github.com/dianedelallee/homebrew-QoQaCommandLine/archive/refs/tags/v0.4.zip", :using => :curl
 
   def install
     bin.install "bin/qoqa_cli"
