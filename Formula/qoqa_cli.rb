64859f69933dadaaab3636b7baa79d975d89b8de
dianedelallee <diane.delallee@gmail.com>
2024-10-07 09:42:54 +0200

    :bug: Fix case when there is no thematics in universe

--- Formula/qoqa_cli.rb
+++ Formula/qoqa_cli.rb
@@ -1,9 +1,9 @@
 class QoqaCli < Formula
   desc "Command Line interface to get information about QoQa (live offers)"
   homepage "https://github.com/dianedelallee/QoQaCommandLine"
-  version "0.8.1"
+  version "0.8.2"
 
-  url "https://github.com/dianedelallee/homebrew-QoQaCommandLine/archive/refs/tags/v0.8.1.zip", :using => :curl
+  url "https://github.com/dianedelallee/homebrew-QoQaCommandLine/archive/refs/tags/v0.8.2.zip", :using => :curl
 
   def install
     bin.install "bin/qoqa_cli"
